1c3161fb369bc9aa07434d6b743f0058c83cfc17
Wado <Ibssa.Wado@seattlecolleges.edu>
2019-04-03 18:59:11 -0700

    changed home.html

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const http = require("http"); 
-const fs = require("fs");
+   const fs = require("fs");
 http.createServer((req,res) => {
   const path = req.url.toLowerCase();
   switch(path) {
     case '/':
-   //   res.writeHead(200, {'Content-Type': 'text/plain'});
+      //res.writeHead(200, {'Content-Type': 'text/plain'});
      // res.end('Home page');
-             
-   fs.readFile("home.html", (err, data) => {
+  case '/':   
+   fs.readFile("public/home.html", (err, data) => {
      if (err) return console.error(err);
      res.writeHead(200, {'Content-Type': 'text/html'});
      res.end(data.toString());
+  });
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
