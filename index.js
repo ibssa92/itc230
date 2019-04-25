fa577a682f62f5f409ee16378fea2824701b2d32
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2019-04-24 18:38:47 -0700

    fix path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require("http"), fs = require('fs'), qs = require("querystring");
-let book = require("../lib/books.js");
+let book = require('./lib/books.js');
 
 
 function serveStatic(res, path, contentType, responseCode){
@@ -25,7 +25,7 @@ http.createServer((req,res) => {
     
   switch(path) {
   case '/': 
-      serveStatic(res, '/../public/home.html', 'text/html');
+      serveStatic(res, '/public/home.html', 'text/html');
       break;
           
     case '/about':
@@ -51,3 +51,119 @@ http.createServer((req,res) => {
       break;
     }
 }).listen(process.env.PORT || 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ASSIGNMENT 3 INDEX.JS
+
+    
+"use strict"; 
+
+let book = require("./public/book.js");
+
+const express = require("express"); 
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.use(require("body-parser").urlencoded({extended: true})); 
+
+//handle bars 
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
+//send static file as a response 
+
+//home page
+app.get('/', function(req,res){
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html');
+}); 
+
+//about page 
+app.get('/about', function(req,res){
+    res.type('text/plain');
+    res.send('About page');
+});
+
+//delete
+app.get('/delete', function(req,res){
+    let result = book.delete(req.query.title); // delete book object
+    res.render('delete', {title: req.query.title, result: result});
+});
+
+//details
+app.post('/get', function(req,res){
+    console.log(req.body);
+    var header = 'Searching for: ' + req.body.title + '<br>';
+    var found = book.get(req.body.title);
+    res.render("details", {title: req.body.title, result: found});
+});
+
+//404 handler
+app.use(function(req,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express Started!'); 
+}); 
+
+*/
+
