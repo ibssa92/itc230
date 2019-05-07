83efa87f646d933a46c958bbda5b472ba28bdcde
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2019-05-06 17:30:42 -0700

    update index

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
-var http = require("http"), fs = require('fs'), qs = require("querystring");
+'use strict'
+
 let book = require('./lib/books.js');
 
+const express = require("express");
+const app = express();
 
-function serveStatic(res, path, contentType, responseCode){
-  if(!responseCode) responseCode = 200;
-  fs.readFile(__dirname + path, function(err, data){
-      if(err){
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-      }
-      else{
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-      }
-  });
-}
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); 
 
-http.createServer((req,res) => {
-  let url = req.url.split("?"); 
-  let query = qs.parse(url[1]); 
-  let path = url[0].toLowerCase();
-    
-    
-    
-  switch(path) {
-  case '/': 
-      serveStatic(res, '/public/home.html', 'text/html');
-      break;
-          
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-          
-    case '/get':
-      let found = book.get(query.title);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results = (found) ? JSON.stringify(found) : "Not found";
-      res.end('Your results for ' + query.title + "\n" + results);
-      break;
-          
-    case '/delete':
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('delete');
-        break;
-          
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+app.use(require("body-parser").urlencoded({extended: true}));
+
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
+//HOME
+app.get('/', function(req,res){
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html'); 
+});
+
+//ABOUT
+app.get('/about', function(req,res){
+    res.type('text/plain');
+    res.send('About page');
+});
+
+//DELETE
+app.get('/delete', function(req,res){
+    let result = book.delete(req.query.title); 
+    res.render('delete', {title: req.query.title, result: result});
+});
+
+//DETAILS
+app.post('/detail', function(req,res){
+    console.log(req.body)
+    var header = 'Searching for: ' + req.body.title + '<br>';
+    var found = book.get(req.body.title);
+    res.render("details", {title: req.body.title, result: found});
+});
+
+// 404
+app.use(function(req,res) {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+//RUN SERVER
+app.listen(app.get('port'), function() {
+    console.log('Express started');    
+});
+
